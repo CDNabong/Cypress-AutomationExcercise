21457b1f6b552ba43b7d5b8eb39f455452b24f30
Christian Nabong <nabong.christiandante@yahoo.com>
2025-02-12 19:04:23 +0800

    created new script for addtocart

---
+++ cypress/e2e/tests/AddToCart.cy.js
@@ -0,0 +1,27 @@
+import { menAddtoCartAllProducts, womenAddtoCartAllProducts, kidsAddtoCartAllProducts } from '../../support/tcm/products.js';
+
+describe('Add to Cart Tests', () => {
+  before(() => {
+    // Setup code before tests run
+
+    //clears the cookies
+    cy.clearCookies();
+    cy.visit('/products');
+    cy.verifyPageTitle('Automation Exercise - All Products');
+  });
+
+  beforeEach(() => {
+    // Code to run before each test
+  });
+
+  afterEach(() => {
+    // Code to run after each test
+  });
+
+  after(() => {
+    // Cleanup code after all tests run
+  });
+  menAddtoCartAllProducts();
+  womenAddtoCartAllProducts();
+  kidsAddtoCartAllProducts();
+});
--- cypress/support/tcm/products.js
+++ cypress/support/tcm/products.js
@@ -1,13 +1,16 @@
 const products = {
   addToCart: "button.btn.btn-default.cart",
+  continueShopping: "[data-dismiss='modal']",
   women: {
     link: "a[href='#Women']",
     dress: {
+      link: "a[href='/category_products/1']",
       sleevelessDress: "a[href='/product_details/3']",
       stylishDress: "a[href='/product_details/4']",
       maxiDress: "a[href='/product_details/38']"
     },
     tops: {
+      link: "a[href='/category_products/2']",
       blueTop: "a[href='/product_details/1']",
       winterTop: "a[href='/product_details/5']",
       whiteTop: "a[href='/product_details/6']",
@@ -16,6 +19,7 @@ const products = {
       laceTop: "a[href='/product_details/42']"
     },
     saree: {
+      link: "a[href='/category_products/7']",
       printSaree: "a[href='/product_details/39']",
       redLinenSaree: "a[href='/product_details/40']",
       blueLinenSaree: "a[href='/product_details/41']",
@@ -72,3 +76,280 @@ const products = {
     biba: "a[href='/brand_products/Biba']"
   }
 }
+
+export const menAddtoCartAllProducts = () => {
+  it('should add men T-Shirt to cart', () => {
+    cy.get(products.men.link).click();
+    cy.get(products.men.tShirts.link).click();
+    cy.get(products.men.tShirts.menTShirt).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add men V-Neck Shirt to cart', () => {
+    cy.get(products.men.link).click();
+    cy.get(products.men.tShirts.link).click();
+    cy.get(products.men.tShirts.vNeckShirt).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add men Polo Shirt to cart', () => {
+    cy.get(products.men.link).click();
+    cy.get(products.men.tShirts.link).click();
+    cy.get(products.men.tShirts.poloShirt).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+  it('should add men Detail Shirt to cart', () => {
+    cy.get(products.men.link).click();
+    cy.get(products.men.tShirts.link).click();
+    cy.get(products.men.tShirts.detailShirt).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add men Green Shirt to cart', () => {
+    cy.get(products.men.link).click();
+    cy.get(products.men.tShirts.link).click();
+    cy.get(products.men.tShirts.greenShirt).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add men Graphic Shirt to cart', () => {
+    cy.get(products.men.link).click();
+    cy.get(products.men.tShirts.link).click();
+    cy.get(products.men.tShirts.graphicShirt).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add men Stretch Jeans to cart', () => {
+    cy.get(products.men.link).click();
+    cy.get(products.men.jeans.link).click();
+    cy.get(products.men.jeans.stretchJeans).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add men Straight Jeans to cart', () => {
+    cy.get(products.men.link).click();
+    cy.get(products.men.jeans.link).click();
+    cy.get(products.men.jeans.straightJeans).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add men Slim Fit Jeans to cart', () => {
+    cy.get(products.men.link).click();
+    cy.get(products.men.jeans.link).click();
+    cy.get(products.men.jeans.slimFitJeans).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+};
+
+export const womenAddtoCartAllProducts = () => {
+  it('should add women Sleeveless Dress to cart', () => {
+    cy.get(products.women.link).click();
+    cy.get(products.women.dress.link).click();
+    cy.get(products.women.dress.sleevelessDress).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add women Stylish Dress to cart', () => {
+    cy.get(products.women.link).click();
+    cy.get(products.women.dress.link).click();
+    cy.get(products.women.dress.stylishDress).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add women Maxi Dress to cart', () => {
+    cy.get(products.women.link).click();
+    cy.get(products.women.dress.link).click();
+    cy.get(products.women.dress.maxiDress).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add women Blue Top to cart', () => {
+    cy.get(products.women.link).click();
+    cy.get(products.women.tops.link).click();
+    cy.get(products.women.tops.blueTop).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add women Winter Top to cart', () => {
+    cy.get(products.women.link).click();
+    cy.get(products.women.tops.link).click();
+    cy.get(products.women.tops.winterTop).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add women White Top to cart', () => {
+    cy.get(products.women.link).click();
+    cy.get(products.women.tops.link).click();
+    cy.get(products.women.tops.whiteTop).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add women Madame Top to cart', () => {
+    cy.get(products.women.link).click();
+    cy.get(products.women.tops.link).click();
+    cy.get(products.women.tops.madameTop).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add women Green Top to cart', () => {
+    cy.get(products.women.link).click();
+    cy.get(products.women.tops.link).click();
+    cy.get(products.women.tops.greenTop).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add women Lace Top to cart', () => {
+    cy.get(products.women.link).click();
+    cy.get(products.women.tops.link).click();
+    cy.get(products.women.tops.laceTop).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add women Print Saree to cart', () => {
+    cy.get(products.women.link).click();
+    cy.get(products.women.saree.link).click();
+    cy.get(products.women.saree.printSaree).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add women Red Linen Saree to cart', () => {
+    cy.get(products.women.link).click();
+    cy.get(products.women.saree.link).click();
+    cy.get(products.women.saree.redLinenSaree).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add women Blue Linen Saree to cart', () => {
+    cy.get(products.women.link).click();
+    cy.get(products.women.saree.link).click();
+    cy.get(products.women.saree.blueLinenSaree).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+};
+
+export const kidsAddtoCartAllProducts = () => {
+  it('should add kids Sleeveless Top and Shorts to cart', () => {
+    cy.get(products.kids.link).click();
+    cy.get(products.kids.dress.link).click();
+    cy.get(products.kids.dress.slevelessTopAndShorts).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add kids Sleeveless Unicorn Dress to cart', () => {
+    cy.get(products.kids.link).click();
+    cy.get(products.kids.dress.link).click();
+    cy.get(products.kids.dress.slevelessUnicornDress).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add kids Embroided Dress to cart', () => {
+    cy.get(products.kids.link).click();
+    cy.get(products.kids.dress.link).click();
+    cy.get(products.kids.dress.embroidedDress).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add kids Mickey Dress to cart', () => {
+    cy.get(products.kids.link).click();
+    cy.get(products.kids.dress.link).click();
+    cy.get(products.kids.dress.mickeyDress).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add kids Pink Dress to cart', () => {
+    cy.get(products.kids.link).click();
+    cy.get(products.kids.dress.link).click();
+    cy.get(products.kids.dress.pinkDress).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add kids Multi Color Dress to cart', () => {
+    cy.get(products.kids.link).click();
+    cy.get(products.kids.dress.link).click();
+    cy.get(products.kids.dress.multiColorDress).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add kids Sleeveless Printed Top to cart', () => {
+    cy.get(products.kids.link).click();
+    cy.get(products.kids.topsAndShirts.link).click();
+    cy.get(products.kids.topsAndShirts.slevelessPrintedTop).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add kids Half Sleeves Pink Top to cart', () => {
+    cy.get(products.kids.link).click();
+    cy.get(products.kids.topsAndShirts.link).click();
+    cy.get(products.kids.topsAndShirts.halfSLeevesPinkTop).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add kids Frozen Top to cart', () => {
+    cy.get(products.kids.link).click();
+    cy.get(products.kids.topsAndShirts.link).click();
+    cy.get(products.kids.topsAndShirts.frozenTop).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add kids Full Sleeves Top to cart', () => {
+    cy.get(products.kids.link).click();
+    cy.get(products.kids.topsAndShirts.link).click();
+    cy.get(products.kids.topsAndShirts.fullSleevesTop).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add kids Printed Off Shoulder Top to cart', () => {
+    cy.get(products.kids.link).click();
+    cy.get(products.kids.topsAndShirts.link).click();
+    cy.get(products.kids.topsAndShirts.printedOffShoulderTop).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add kids Panda Shirt to cart', () => {
+    cy.get(products.kids.link).click();
+    cy.get(products.kids.topsAndShirts.link).click();
+    cy.get(products.kids.topsAndShirts.pandaShirt).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+
+  it('should add kids Blocked Shirt to cart', () => {
+    cy.get(products.kids.link).click();
+    cy.get(products.kids.topsAndShirts.link).click();
+    cy.get(products.kids.topsAndShirts.blockedShirt).click();
+    cy.get(products.addToCart).click();
+    cy.get(products.continueShopping).click();
+  });
+};
