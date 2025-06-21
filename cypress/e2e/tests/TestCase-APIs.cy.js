1a66bb56571dfe1931dd64ecf49546d3e13724ac
Christian Nabong <nabong.christiandante@yahoo.com>
2025-06-21 22:24:44 +0800

    added delete API and fixed the scenario for E2E testing

--- cypress/e2e/tests/TestCase-APIs.cy.js
+++ cypress/e2e/tests/TestCase-APIs.cy.js
@@ -50,23 +50,16 @@ describe('Verify API tests', () => {
     });
   })
   */
-
-  it('API 7: POST To Verify Login with valid details', () => {
-    cy.request({
-      method: 'POST',
-      url: `${apiUrl}verifyLogin`,
-      form: true,
-      body: {
-        email: 'testclient2@yopmail.com',
-        password: 'client',
-      }
-    }).then((response) => {
-      const verifyValidLoginResponse = JSON.stringify(response.body);
-      expect(response.status).to.eq(200);
-      expect(verifyValidLoginResponse).to.include(200); // check if the response contains 200
-      expect(verifyValidLoginResponse).to.include('User exists!'); // check if the response contains 'User exists!'
-    });
-  })
+  const firstName = faker.person.firstName(); // Generate a random first name
+  const lastName = faker.person.lastName(); // Generate a random last name
+  const fullName = `${firstName} ${lastName}`;  // Combine the first and last name into full name
+  const email = `${firstName.toLowerCase()}.${lastName.toLowerCase()}@yopmail.com`; // Generate a random email
+  const password = faker.internet.password(); // Generate a random password
+  const mobile = faker.phone.number({ style: 'national' }); // Generate random phone number
+  const streetAddress = faker.location.streetAddress(); // Random street address
+  const city = faker.location.city(); // Random city
+  const state = faker.location.state(); // Random state
+  const zipCode = faker.location.zipCode(); // Random zip code
 
   it('API 10: POST To Verify Login with invalid details', () => {
     cy.request({
@@ -74,8 +67,8 @@ describe('Verify API tests', () => {
       url: `${apiUrl}verifyLogin`,
       form: true,
       body: {
-        email: 'testclient2@yopmail.com',
-        password: 'clients',
+        email: email,
+        password: password,
       }
     }).then((response) => {
       const verifyInvalidLoginResponse = JSON.stringify(response.body)
@@ -86,16 +79,6 @@ describe('Verify API tests', () => {
   })
 
   it('API 11: POST To Create/Register User Account', () => {
-    const firstName = faker.person.firstName(); // Generate a random first name
-    const lastName = faker.person.lastName(); // Generate a random last name
-    const fullName = `${firstName} ${lastName}`;  // Combine the first and last name into full name
-    const email = `${firstName.toLowerCase()}.${lastName.toLowerCase()}@yopmail.com`; // Generate a random email
-    const password = faker.internet.password(); // Generate a random password
-    const mobile = faker.phone.number({ style: 'national' }); // Generate random phone number
-    const streetAddress = faker.location.streetAddress(); // Random street address
-    const city = faker.location.city(); // Random city
-    const state = faker.location.state(); // Random state
-    const zipCode = faker.location.zipCode(); // Random zip code
     cy.request({
       method: 'POST',
       url: `${apiUrl}createAccount`,
@@ -124,6 +107,40 @@ describe('Verify API tests', () => {
       expect(response.status).to.eq(200);
       expect(verifyCreatedUserResponse).to.include(201); // check if the response contains '201'
       expect(verifyCreatedUserResponse).to.include('User created!'); // check if the response contains 'User created!'
+    });
+  })
+
+  it('API 7: POST To Verify Login with valid details', () => {
+    cy.request({
+      method: 'POST',
+      url: `${apiUrl}verifyLogin`,
+      form: true,
+      body: {
+        email: email,
+        password: password,
+      }
+    }).then((response) => {
+      const verifyValidLoginResponse = JSON.stringify(response.body);
+      expect(response.status).to.eq(200);
+      expect(verifyValidLoginResponse).to.include(200); // check if the response contains 200
+      expect(verifyValidLoginResponse).to.include('User exists!'); // check if the response contains 'User exists!'
+    });
+  })
+
+  it('API 12: DELETE METHOD To Delete User Account', () => {
+    cy.request({
+      method: 'DELETE',
+      url: `${apiUrl}deleteAccount`,
+      form: true,
+      body: {
+        email: email,
+        password: password,
+      }
+    }).then((response) => {
+      const verifyDeleteResponse = JSON.stringify(response.body)
+      expect(response.status).to.eq(200);
+      expect(verifyDeleteResponse).to.include(200); // check if the response contains 404
+      expect(verifyDeleteResponse).to.include('Account deleted!'); // check if the response contains 'User not found!'
     });
   })
 
